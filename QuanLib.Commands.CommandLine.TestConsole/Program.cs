78d0f5e3938a04822b73492fed150441e7bfd689
Quan2 <2200191123@qq.com>
2024-05-06 18:24:34 +0800

    修正命名空间

--- QuanLib.Commands.CommandLine.TestConsole/Program.cs
+++ QuanLib.Commands.CommandLine.TestConsole/Program.cs
@@ -10,7 +10,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuanLib.Core.TestConsole
+namespace QuanLib.Commands.CommandLine.TestConsole
 {
     public static class Program
     {
--- QuanLib.Consoles.TestConsole/Program.cs
+++ QuanLib.Consoles.TestConsole/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuanLib.Core.TestConsole
+namespace QuanLib.Consoles.TestConsole
 {
     public static class Program
     {
