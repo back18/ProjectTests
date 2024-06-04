845ebbd623e3f5182be9ff296349a63ecbedeed6
Quan <2200191123@qq.com>
2024-06-04 21:58:11 +0800

    添加 QuanLib.VisualObject.TestConsole 控制台测试项目

---
+++ QuanLib.VisualObject.TestConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.VisualObject.TestConsole
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            File.WriteAllText("test.txt", "hello, world!");
+            using FileStream fileStream = File.OpenRead("test.txt");
+            string text = new TypeView(fileStream.GetType(), fileStream).ToString();
+            Console.WriteLine(text);
+        }
+    }
+}
